0d07e10fcc35f0f4114b049d3349b38d7a883005
Abinash2249 <abinashdahal2001@gmail.com>
2024-01-24 16:27:09 +0545

    Changes made

---
+++ day6/2_string_formatting.py
@@ -0,0 +1,9 @@
+name = "Python"
+name2 = "Java"
+data = f"I am learning {name}. I am also leaning {name2}"  # This  is called as f-string
+print(data)     # I am learning Python. I am also learning Java.
+
+data = "I am learning {}. I am also learning {}".format(name, name2)
+print(data)     # I am learning Python. I am also learning Java.
+
+
