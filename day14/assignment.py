a3b43d2693527c3336ad22ee9e17c01855e214f5
Abinash2249 <abinashdahal2001@gmail.com>
2024-02-06 15:03:57 +0545

    Updated day15

--- day14/assignment.py
+++ day14/assignment.py
@@ -2,21 +2,35 @@
 
 
 
+def execution_time(funct):
+    def inner_function(*args, **kwargs):
+        import time
+        start = time.time()
+        result = funct(*args, **kwargs)
+        end = time.time()
+        elapsed_time = end - start
+        print(f"The total elapsed time to execute the function was: {elapsed_time}")
+        return result
+    return inner_function
+
+
+
 @execution_time
 def func():
     for i in range(1000000000):
         continue
     print(("Loop complete !!"))
 
+
 func()
 
 
-# import time
-# start = time.time()
-# a = 1
-# b = 2
-# print(a+b)
-# end = time.time()
-# total_time = end - start
-# print(total_time)
+import time
+start = time.time()
+a = 1
+b = 2
+print(a+b)
+end = time.time()
+total_time = end - start
+print(total_time)
 
